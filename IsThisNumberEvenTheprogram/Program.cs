c5b3edd3a8f8ae42f56131f997bcea7e57e0edbe
Gerevini <168560762+Gerevini@users.noreply.github.com>
2024-07-06 21:02:31 -0300

    Add files via upload

---
+++ IsThisNumberEvenTheprogram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste2
+{
+    internal class Program
+    {
+        static string Even(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "This number is even";
+            }
+            else
+            {
+                return "This number is odd";
+            }
+        }
+        static void Main(string[] args)
+        {
+            int number = 3;
+            Console.WriteLine(Even(number));
+
+        }
+    }
+}
+
