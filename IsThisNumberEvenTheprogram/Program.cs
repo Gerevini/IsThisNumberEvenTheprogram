aac68a9143b996bf85300fd1c1cdc687cc6f343d
Gerevini <168560762+Gerevini@users.noreply.github.com>
2024-07-13 15:35:27 -0300

    Update Program.cs

--- IsThisNumberEvenTheprogram/Program.cs
+++ IsThisNumberEvenTheprogram/Program.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Teste2
 {
